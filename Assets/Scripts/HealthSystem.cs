356a9ad95800fd3cf982cf5e0be023fa10e0682b
LCUSTOS <lucas.custos@ynov.com>
2023-03-06 11:46:28 +0100

    some fixes

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -3,20 +3,22 @@ using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class HealthSystem : MonoBehaviour
 {
     public Sprite redbull, redbullEmpty;
     public Image life1, life2, life3;
+    private UI ui;
     
     void Start()
     {
-        
+        ui = GameObject.Find("UI").GetComponent<UI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        switch (PlayerLife.instance.Health)
+        switch (ui.health)
         {
             case 0:
                 life1.sprite = redbullEmpty;
