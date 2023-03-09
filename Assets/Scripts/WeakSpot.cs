ce2aeb804f1d8077bed3fbe50f6916115cc34050
LCUSTOS <lucas.custos@ynov.com>
2023-03-09 10:33:44 +0100

    little change

--- Assets/Scripts/WeakSpot.cs
+++ Assets/Scripts/WeakSpot.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 public class WeakSpot : MonoBehaviour
 {
     public GameObject objectToDestroy;
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             Destroy(objectToDestroy);
+            print("touch");
         }
     }
 }
